d0b57de2809e62ce7b58bb3b06a1d8a6d6a7167a
harshv3001 <harsh1999verma.hv@gmail.com>
2021-10-10 19:45:15 +0530

    tailwind changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   node: "jit",
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  purge: {
+    content: [
+      "./src/pages/**/*.{js,jsx,ts,tsx}",
+      "./src/components/**/*.{js,jsx,ts,tsx}",
+    ],
+    // These options are passed through directly to PurgeCSS
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {},
   variants: {
